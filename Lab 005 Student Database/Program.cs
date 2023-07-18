d501b1f07d9a021554b8bd2a49ba2b9e97e9a24c
Maria Ragone <maria@ragone.net>
2023-07-18 01:55:50 -0400

    version 3 - almost there

--- Lab 005 Student Database/Program.cs
+++ Lab 005 Student Database/Program.cs
@@ -1,4 +1,8 @@
-﻿using System.Transactions;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Reflection;
+using System.Transactions;
+using System.Xml.Linq;
 
 Console.WriteLine("Welcome to the Student Database.");
 Console.WriteLine();
@@ -17,6 +21,12 @@ string[] names =
     "Timothy Montague", 
     "Maria Ragone" 
 };
+
+
+for (int i = 0; i < names.Length; i++)
+{
+    names[i] = names[i].ToLower();
+}
 string[] towns = 
     { 
     "Columbus", 
@@ -49,76 +59,117 @@ string[] foods =
 //VARIABLES
 bool search = true;
 int choice = 0;
-//provide an option to see a list of all the students    
-//only show the list of names once at the beginning to avoid repetition
+bool found = false;
+
 while (search)
-{ 
-    while (true)
 {
-    Console.WriteLine("Do you want so search by name or number?");
-    string nameOrNumber = (Console.ReadLine().Trim().ToLower());
+    Console.WriteLine("Do you want to search by name or number?");
+    string nameOrNumber = Console.ReadLine().Trim().ToLower();
+
     if (nameOrNumber == "name")
     {
         Console.WriteLine("Enter a name to search.");
-        string searchName = (Console.ReadLine().Trim().ToLower());
-        int index = Array.IndexOf(names, searchName);
+        string searchName = Console.ReadLine().Trim().ToLower();
 
-        if (index >= 0)
+        for (int n = 0; n < names.Length; n++) //loops thru each value in array
         {
-            Console.WriteLine($"{searchName} found at index {index}");
+            names[n] = names[n].ToLower(); //converts the array values to lowercase
+            if (names[n] == searchName)
+            {
+                choice = n;
+                //Console.WriteLine(choice); //only displays the index location (prints the location of the searchName input)
+                //Console.WriteLine(names[n]); //displays the name of the searchName input
+                while (true)
+                {
+                    Console.WriteLine($"Enter the category to display: Hometown or Favorite Food");
+                    string category = Console.ReadLine().Trim().ToLower();
+                    if (category == "hometown" || category == "town" || category == "home" || category == "h")
+                    {
+                        Console.WriteLine(towns[choice]);
+                        break;
+                    }
+                    else if (category == "food" || category == "favorite" || category == "favorite food" || category == "f")
+                    {
+                        Console.WriteLine(foods[choice]);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("That is not a valid choice. Please try again.");
+                        continue;
+                    }
+                }
+                //ask to continue viewing another student
+                while (true)
+                {
+                    Console.WriteLine($"Would you like to learn about another student? y/n");
+                    string answer = Console.ReadLine().ToLower().Trim();
+                    if (answer == "n")
+                    {
+                        Console.WriteLine($"Bye!"); //exit the program
+                        search = false;
+                        break;
+                    }
+                    else if (answer == "y")
+                    {
+                        search = true;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please type 'y' or 'n'");
+                    }
+                }
+            }
         }
-        else
-        {
-            Console.WriteLine($"{searchName} not found in the array");
-        }
-
-    }
-    else
-    {
         break;
     }
-}
-while (true)
-{
-    Console.WriteLine($"Do you want to see a list of all the students? y/n");
-    string nameList = (Console.ReadLine());
-    if (nameList == "y")
+    else if (nameOrNumber == "number")
     {
-        for (int i = 0; i < names.Length; i++)
+        while (true)
         {
-            Console.WriteLine($"{i}. {names[i]}");
-        }
-        search = true;
-        break;
+            Console.WriteLine($"Do you want to see a list of all the students? y/n");
+            string nameList = (Console.ReadLine());
+            if (nameList == "y")
+            {
+                for (int i = 0; i < names.Length; i++)
+                {
+                    Console.WriteLine($"{i}. {names[i]}");
+                }
+                search = true;
 
+                break;
+
+            }
+            else if (nameList == "n")
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("That is not a valid entry.");
+            }
+        }
+        
+        while (true)
+        {
+            Console.WriteLine($"Enter a student number: 0 - {names.Length - 1}");
+            choice = int.Parse(Console.ReadLine());
+            if (choice >= 0 && choice <= names.Length - 1) //make sure the user inputs a valid entry
+            {
+                Console.WriteLine(names[choice]);
+                break;
+            }
+            else
+            {
+                Console.WriteLine("That is not a valid entry.");
+                search = true;
+            }
+        }
     }
-    else if (nameList == "n")
-    {
-        break;
-    }
-    else
-    {
-        Console.WriteLine("That is not a valid entry.");
-    }
-}
-//search for the student
+    //search for the student
 
     
-    while (true)
-    {
-        Console.WriteLine($"Enter a student number: 0 - {names.Length - 1}");
-        choice = int.Parse(Console.ReadLine());
-        if (choice >= 0 && choice <= names.Length - 1) //make sure the user inputs a valid entry
-        {
-            Console.WriteLine(names[choice]);
-            break;
-        }
-        else
-        {
-            Console.WriteLine("That is not a valid entry.");
-            search = true;
-        }
-    } 
     while (true)
     {
         Console.WriteLine($"Enter the category to display: Hometown or Favorite Food");
@@ -160,5 +211,6 @@ while (true)
             Console.WriteLine("Please type 'y' or 'n'");
         }
     }
-
 }
+
+
